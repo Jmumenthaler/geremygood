b571d2d7301c61dc354299d0044448c51b4f0ad1
Jeremy Mumenthaler <jeremy@simplehonestwork.com>
2013-01-26 01:25:51 -0600

    added this years books

--- books/outliers.php
+++ books/outliers.php
@@ -4,8 +4,9 @@
 <h4>Malcom Gladwell</h4>
 
 <p><span class="drop">A</span>fter reading this book, I really want to change the education system. It seems that possibility lies within everyone, but it's through our opourtunites that we find success. Malcom argues that behind every master is at least 10,000 hours of practice and a slew of opourtunities which made sucess possible.</p>
-<p>This realization of hard work matched with privlege inspired me. I have to get out there, and work twice as hard as I currently am, and make sure I'm taking advantage of every opourtunity that comes my way. These are gifts, just for me, to succeed. </p>
-<p>I also then felt a little disapointed with much of the world. Why doesn't everyone want to spend 10,000 hours working hard on something? Where are the every man's passions? I feel like there are so many people who just exist. </p>
+<p>This realization of hard work matched with privlege inspired me. I have to get out there, and work twice as hard as I currently am, and make sure I'm taking advantage of every opourtunity that comes my way. These are gifts, just for me, to&nbsp;succeed.</p>
+<p>I also then felt a little disapointed with much of the world. Why doesn't everyone want to spend 10,000 hours working hard on something? Where are the every man's passions? I feel like there are so many people who just exist. I want to find a way to inspire those&nbsp;people.</p>
+<p>It pays to be the outlier, the one who had the opourtunity to shine. Get in tune with your passion, and spend the time it takes to become a&nbsp;master. </p>
 
 <h6> 01|01|13 – 01|23|13</h6>
 </article>
